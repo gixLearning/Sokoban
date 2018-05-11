e6281513d8513bf5255da8473833ba15af25636d
gixLearning <38102077+gixLearning@users.noreply.github.com>
2018-05-11 15:48:46 +0200

    Added some block collision

--- js/script.js
+++ js/script.js
@@ -6,7 +6,13 @@ class Player {
         this.Y = y
     }
 }
+
+var direction = { "UP" : 'UP', "DOWN" : 'DOWN', "LEFT" : 'LEFT', "RIGHT": 'RIGHT'}
+Object.freeze(direction);
+
 var player;
+var destinationTileType;
+
 var map = tileMap;
 
 document.addEventListener('keydown', function(event){
@@ -15,35 +21,28 @@ document.addEventListener('keydown', function(event){
     var x, y;
     switch (key) {
         case "ArrowDown":
-            console.log(key);
             x = 1;
             y = 0;
-            moveToCell(x, y);
+            moveToCell(x, y, direction.DOWN);
             break;
         case "ArrowUp":
-            console.log(key)
             x = -1;
             y = 0;
-            moveToCell(x, y);
+            moveToCell(x, y, direction.UP);
             break;
         case "ArrowLeft":
-            console.log(key)
             x = 0;
             y = -1;
-            moveToCell(x, y);
+            moveToCell(x, y, direction.LEFT);
             break;
         case "ArrowRight":
-            console.log(key)
             x = 0;
             y = 1;
-            moveToCell(x, y);
+            moveToCell(x, y, direction.RIGHT);
             break;
-
         default:
         break;
     }
-
-    console.log(player, x, y)
 })
 
 function setUpGame() {
@@ -59,8 +58,11 @@ function setUpGame() {
             var tileType = map.mapGrid[i][j].toString();
             if(tileType === " "){            
                 continue;
-            } else {
-                cell.appendChild(paintBlock(tileType));
+            } else if(tileType === "G"){
+                cell.classList.add('goal-node');
+            } 
+            else {
+                cell.appendChild(paintBlock(tileType, direction.UP));
             }
 
             if(tileType === "P"){
@@ -71,39 +73,129 @@ function setUpGame() {
     populateData();
 }
 
-function moveToCell(x, y){
+function moveToCell(x, y, direction){
     var x1 = player.X + x;
-    var y1 = player.Y + y;
-    console.log("New value: " + x1 + " " + y1)
+    var y1 = player.Y + y;    
+    
+    if(!checkWallCollision(x1, y1)){
+        return;
+    } 
+
+    if(!checkBlockCollision(x1, y1, direction)){
+        return;        
+    }
+
+    //console.log("New value: " + x1 + " " + y1)
+
+    //Nothing is blocked, get on with it!    
+    destinationTileType = map.mapGrid[x1][y1][0];
+    if(destinationTileType === "B"){
+        console.log("PUSH BLOCK");
+        pushBlock(x1, y1, direction);
+    }    
 
     var table = document.getElementById("table-board");
     var cell = table.rows[x1].cells[y1]
-    console.log(cell)
+    //console.log(cell)
+    //console.log(map.mapGrid[x1][y1])
 
-    if(!checkCollision(x1, y1)){
-        return;
-    }
+    
 
-    cell.style.background = "green";
-    console.log(map.mapGrid[x1][y1][0])
-    if(map.mapGrid[x1][y1][0] === "G"){
-        cell.style.background = "red";
-    }   
-
+    //All checks passed, assign new values    
     player.X = x1;
-    player.Y = y1;   
+    player.Y = y1;
 
     map.mapGrid[x1][y1][0] = "P";
+
+
+    //Paint all new tiles
+    var tileType = map.mapGrid[x1][y1].toString();
+    cell.appendChild(paintBlock(tileType, direction))
+
+    //Check if the previous tile was a tile occupied by a player, if so, remove it
+    if(map.mapGrid[x1 - x][y1 - y][0] === "P"){
+        map.mapGrid[x1 - x][y1 - y][0] = ' '
+
+        var oldCell = table.rows[x1 - x].cells[y1 - y];
+
+        for (let index = 0; index < oldCell.childNodes.length; index++) {
+
+            var element = oldCell.childNodes[index];
+            if(element.getAttribute('id') == 'player'){
+                oldCell.removeChild(element);
+            }                                 
+        }
+    }
 }
 
-function checkCollision(x, y){
+function pushBlock(x, y, direction){
+    var table = document.getElementById("table-board");    
+    var cell = table.rows[x].cells[y]
+
+    if(direction == "LEFT"){
+        map.mapGrid[x][y - 1][0] = "B";
+         
+    } 
+    else if (direction == "RIGHT"){
+        map.mapGrid[x][y + 1][0] = "B";
+    }
+    else if (direction == "UP"){
+        map.mapGrid[x - 1][y][0] = "B";
+    } else {
+        map.mapGrid[x + 1][y][0] = "B";
+    }
+}
+
+function checkBlockCollision(x, y, direction){
+    var dir = direction;
+
+    if(map.mapGrid[x][y][0] === "B"){
+        switch (dir) {
+            case "LEFT":
+                if(map.mapGrid[x][y - 1][0] === "B" || map.mapGrid[x][y - 1][0] === "W"){
+                    //console.log("BLOCKED BY " + map.mapGrid[x][y - 1][0])
+                    return false;
+                }
+                break;
+
+            case "RIGHT":
+                if(map.mapGrid[x][y + 1][0] === "B" || map.mapGrid[x][y + 1][0] === "W"){
+                    //console.log("BLOCKED BY " + map.mapGrid[x][y + 1][0])
+                    return false;
+                }                         
+                break;
+
+            case "UP":
+                if(map.mapGrid[x - 1][y][0] === "B" || map.mapGrid[x - 1][y][0] === "W"){
+                    //console.log("BLOCKED BY " + map.mapGrid[x - 1][y][0])
+                    return false;
+                }
+                break;
+
+            case "DOWN":
+                if(map.mapGrid[x + 1][y][0] === "B" || map.mapGrid[x + 1][y][0] === "W"){
+                    //console.log("BLOCKED BY " + map.mapGrid[x + 1][y][0])
+                    return false;
+                }
+                break;
+
+            default:                
+                return false;
+                break;
+        }
+    }
+    return true;
+}
+
+function checkWallCollision(x, y){
     if(map.mapGrid[x][y][0] === "W"){
         return false;
     }
     return true;
 }
 
-function paintBlock(blockChar) {
+function paintBlock(blockChar, d) {    
+    var dir = d;    
 
     var imageSource;
     var isPlayerBlock = false;
@@ -114,14 +206,22 @@ function paintBlock(blockChar) {
         case "B":
             imageSource = "crate_06";
             break;
-        case "G":
-            imageSource = "environment_02";
-            break;
         case "E":
             imageSource = "PlayerFace_dark"
             break; 
-        case "P":
-            imageSource = "player/player_05"
+        case "P":           
+            if(dir == direction.UP){
+                imageSource = "player/player_08"
+            };
+            if(dir == direction.DOWN){
+                imageSource = "player/player_05"
+            }
+            if(dir == direction.LEFT){
+                imageSource = "player/player_20"
+            }
+            if(dir == direction.RIGHT){
+                imageSource = "player/player_17"
+            }            
             isPlayerBlock = true;
             break;   
         default:
@@ -154,7 +254,7 @@ function populateData() {
             {
                 rIndex = this.parentElement.rowIndex;
                 cIndex = this.cellIndex //+1;
-                console.log("Row : "+rIndex+" , Cell : "+cIndex);
+                console.log("Row : "+rIndex+" , Cell : "+cIndex + " MAP: " + map.mapGrid[rIndex][cIndex][0]);
             };
         }  
     }    
